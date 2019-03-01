c2fd90efe95a4a70c2b8523c394b153c3e3fe22f
Masaki <mk5986@nyu.edu>
2019-02-28 20:08:32 -0500

    swtich to petshop box

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,11 @@
-const path = require("path");
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
