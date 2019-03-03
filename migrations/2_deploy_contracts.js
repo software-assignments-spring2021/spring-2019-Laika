b660f4abb57693ca94f826616893e07f275b32de
Masaki <mk5986@nyu.edu>
2019-03-03 14:19:48 -0500

    fix: Laikoin basic ERC20 complete

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,13 @@
 var Adoption = artifacts.require("Adoption");
 var SafeMath    = artifacts.require("node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol");
 var Pausable    = artifacts.require("node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol");
+var Ownable    = artifacts.require("node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol");
 var Laikoin = artifacts.require("Laikoin")
 
 module.exports = function(deployer) {
     deployer.deploy(Adoption);
     deployer.deploy(SafeMath);
     deployer.deploy(Pausable);
+    deployer.deploy(Ownable);
     deployer.deploy(Laikoin);
 };
