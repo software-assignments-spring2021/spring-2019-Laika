82dd74ec988d746391460c267a6fa1744e59d50c
Rachel Rios <rnr266@nyu.edu>
2019-03-04 14:41:15 -0500

    Home front end, Market Place basics, Charity front end

--- src/js/main.js
+++ src/js/main.js
@@ -56,7 +56,7 @@ jQuery(document).ready(function($){
 
 	function toggleCart(bool) {
 		var cartIsOpen = ( typeof bool === 'undefined' ) ? cartWrapper.hasClass('cart-open') : bool;
-		
+
 		if( cartIsOpen ) {
 			cartWrapper.removeClass('cart-open');
 			//reset undo
@@ -78,7 +78,7 @@ jQuery(document).ready(function($){
 		var cartIsEmpty = cartWrapper.hasClass('empty');
 		//update cart product list
 		addProduct();
-		//update number of items 
+		//update number of items
 		updateCartCount(cartIsEmpty);
 		//update total price
 		updateCartTotal(trigger.data('price'), true);
@@ -91,18 +91,18 @@ jQuery(document).ready(function($){
 		//you should insert an item with the selected product info
 		//replace productId, productName, price and url with your real product info
 		productId = productId + 1;
-		var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div><div class="product-details"><h3><a href="#0">Product Name</a></h3><span class="price">$25.99</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
+		var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div><div class="product-details"><h3><a href="#0">Product Name</a></h3><span class="price">$2.99</span><div class="actions"><a href="#0" class="delete-item">Delete</a><div class="quantity"><label for="cd-product-'+ productId +'">Qty</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
 		cartList.prepend(productAdded);
 	}
 
 	function removeProduct(product) {
 		clearInterval(undoTimeoutId);
 		cartList.find('.deleted').remove();
-		
+
 		var topPosition = product.offset().top - cartBody.children('ul').offset().top ,
 			productQuantity = Number(product.find('.quantity').find('select').val()),
 			productTotPrice = Number(product.find('.price').text().replace('$', '')) * productQuantity;
-		
+
 		product.css('top', topPosition+'px').addClass('deleted');
 
 		//update items count + total price
@@ -120,7 +120,7 @@ jQuery(document).ready(function($){
 	function quickUpdateCart() {
 		var quantity = 0;
 		var price = 0;
-		
+
 		cartList.children('li:not(.deleted)').each(function(){
 			var singleQuantity = Number($(this).find('select').val());
 			quantity = quantity + singleQuantity;
@@ -136,7 +136,7 @@ jQuery(document).ready(function($){
 		if( typeof quantity === 'undefined' ) {
 			var actual = Number(cartCount.find('li').eq(0).text()) + 1;
 			var next = actual + 1;
-			
+
 			if( emptyCart ) {
 				cartCount.find('li').eq(0).text(actual);
 				cartCount.find('li').eq(1).text(next);
@@ -158,7 +158,7 @@ jQuery(document).ready(function($){
 		} else {
 			var actual = Number(cartCount.find('li').eq(0).text()) + quantity;
 			var next = actual + 1;
-			
+
 			cartCount.find('li').eq(0).text(actual);
 			cartCount.find('li').eq(1).text(next);
 		}
