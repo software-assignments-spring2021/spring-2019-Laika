2a5840265a688310d04773be4cf7de8963ffe833
owenofengland <joe215@nyu.edu>
2019-03-04 18:38:27 -0500

    testBalance

--- test/TestLaikoin.sol
+++ test/TestLaikoin.sol
@@ -5,10 +5,19 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Laikoin.sol";
 
 contract TestLaikoin {
+	Laikoin laikoin = Laikoin(DeployedAddresses.Laikoin());
+	address expectedOwner = address(this);
+	uint256 expectedBalance = 0;
+
 	function testTotalSupply() public {
 		Laikoin lai = new Laikoin();
 		uint expected = 7500000;
 		uint result = lai.totalSupply();
 		Assert.equal(result, expected, "Initial total supply should be 7500000");
+	}
+
+	function testBalanceOf() public {
+		uint256 returnedBalance = laikoin.balanceOf(expectedOwner);
+		Assert.equal(returnedBalance, expectedBalance, "Returned balance should equal expected balance, 0");
 	}
 }
