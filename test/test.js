55d40a869b3d460beb812a4032f21f30c28cfcda
sillyangela <qz625@nyu.edu>
2019-03-05 08:35:47 -0500

    contact me

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+//var x = require("check");
+
+describe("", function(){
+    it("checks to see if all fields were filled out", function(done){
+        assert.equal(isComplete(), true);
+        done();
+    });
+});
+
