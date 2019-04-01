ccef44e99d19886b000b8cdc8059382e22082b02
Masaki <mk5986@nyu.edu>
2019-03-31 22:27:08 -0400

    add: get product info tests

--- test/TestMarket.sol
+++ test/TestMarket.sol
@@ -7,63 +7,36 @@ import "truffle/DeployedAddresses.sol";
 
 contract TestMarket {
 
+    // Market _market = Market(DeployedAddresses.Market());
+
     function beforeAll() {
         market = new Market();
         dummyAddress = address(0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA);
         customerAddress = address(0x00FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFB);
+        market.registerProduct(5, "test product", "test description", 22);
     }
 
-    function testGetProductInfo() public {
-        Market _market = Market(DeployedAddresses.Market());
-        uint256 expected = 0;
-        Assert.equal();
+    function testRegisterProduct() {
+        bool expected = true;
+        bool result = market.registerProduct(20, "dogfood", "it is very tasty dogfood", 99);
+        Assert.equal(result, expected, "Market should register a product");
+    }
+
+    function testGetProductInfoName() public {
+        bytes32 expected_product_name = bytes32("test product");
+        var (_name, _description, _price) = market.getProduct(5);
+        Assert.equal(_name, expected_product_name, "Market should return product name");
+    }
+
+    function testGetProductInfoDescription() public {
+        bytes32 expected_product_description = bytes32("test description");
+        var (_name, _description, _price) = market.getProduct(5);
+        Assert.equal(_name, expected_product_description, "Market should return product description");
+    }
+
+    function testGetProductInfoPrice() public {
+        uint256 expected_product_price = 22;
+        var (_name, _description, _price) = market.getProduct(5);
+        Assert.equal(_name, expected_product_price, "Market should return product price");
     }
 }
-
-	Laikoin laikoin = Laikoin(DeployedAddresses.Laikoin());
-	address expectedOwner = address(this);
-
-	function testTotalSupply() public {
-		Laikoin lai = new Laikoin();
-		uint expected = 7500000;
-		uint result = lai.totalSupply();
-		Assert.equal(result, expected, "Initial total supply should be 7500000");
-	}
-
-	function testGetAddress() public {
-		address returnedAddress = laikoin.getAddress();
-		Assert.equal(returnedAddress, expectedOwner, "Returned address should be equal to expected");
-	}
-	/*
-	function testTransfer() public {
-		ExposedLaikoin user1 = new ExposedLaikoin();
-		ExposedLaikoin user2 = new ExposedLaikoin();
-		address user1Addr = user1.getAddress();
-		address user2Addr = user2.getAddress();
-		user1.mint(user1Addr, 1000);
-		bool success = user1.transfer(user2Addr, 400);
-		Assert.equal(success, true, "transfer should succeed");
-		Assert.equal(user1.balanceOf(user1Addr), 600, "User1 should have 500 tokens");
-		Assert.equal(user2.balanceOf(user2Addr), 400, "User2 should have 500 tokens");
-	}
-	*/
-
-	function testMint() public {
-		ExposedLaikoin user1 = new ExposedLaikoin();
-		address user1Addr = user1.getAddress();
-		user1.mint(user1Addr, 1000);
-		Assert.equal(user1.balanceOf(user1Addr), 2000, "User1 should have 2000 tokens now");
-	}
-
-	function testBalanceOf() public {
-		uint256 expectedBalance = 0;
-		uint256 returnedBalance = laikoin.balanceOf(expectedOwner);
-		Assert.equal(returnedBalance, expectedBalance, "Returned balance should equal expected balance, 0");
-	}
-
-	function testApprove() public {
-		Laikoin user1 = new Laikoin();
-		bool expectedSuccess = true;
-		bool resultSuccess = user1.approve(expectedOwner, 0);
-		Assert.equal(resultSuccess, expectedSuccess, "User 1 approving user 2 sending 0 tokens should return true");
-	}
