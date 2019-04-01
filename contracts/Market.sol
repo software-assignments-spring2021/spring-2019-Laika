479dac5a36ea3a3624c5494937e2f2764a8c7a6f
Masaki <mk5986@nyu.edu>
2019-03-31 21:00:37 -0400

    variables and events

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract Market {
-    
+    bytes32 public store_name;
+    uint256 public store_balance;
+
+    mapping (address => Customer) customers;
+    mapping (uint256 => Product) products;
+
+    event PurchaseCompleted(address customer, uint256 payment);
+    event PurchaseFailed(address customer, uint256 customerBalance, uint256 payment);
 }
