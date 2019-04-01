5fed0d0042c4459705cc571e9a810d6a6e145564
Masaki <mk5986@nyu.edu>
2019-03-31 21:39:17 -0400

    add: functions

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -23,5 +23,26 @@ contract Market {
         uint256 price;
     }
 
-    
+    // ether payable fallback
+    function() external payable {}
+
+    function getBalance() public view returns (uint256 _balance) {
+        return customers[msg.sender].balance;
+    }
+
+    function getProductInfo(uint256 id) public view returns (bytes32 name, bytes32 description, uint256 price) {
+        return (products[id].name, products[id].description, products[id].price);
+    }
+
+    function buyProduct(uint256 id) public returns (bool success) {
+        Customer memory customer = customers[msg.sender];
+        Product memory product = products[id];
+        if ((customer.balance >= product.price)) {
+            customer.balance -= product.price;
+            emit PurchaseCompleted(msg.sender, product.price);
+            return true;
+        }
+        emit PurchaseFailed(msg.sender, customer.balance, product.price);
+        return false;
+    }
 }
