bf82de24c729f24916b922a7f01b5f69b9b6d305
Masaki <mk5986@nyu.edu>
2019-04-01 21:00:03 -0400

    fix name

--- test/TestMarket.sol
+++ test/TestMarket.sol
@@ -33,19 +33,19 @@ contract TestMarket {
 
     function testGetProductInfoName() public {
         bytes32 expected_product_name = bytes32("test product");
-        var (_name, _description, _price) = market.getProduct(5);
+        var (_name, _description, _price) = market.getProductInfo(5);
         Assert.equal(_name, expected_product_name, "Market should return product name");
     }
 
     function testGetProductInfoDescription() public {
         bytes32 expected_product_description = bytes32("test description");
-        var (_name, _description, _price) = market.getProduct(5);
+        var (_name, _description, _price) = market.getProductInfo(5);
         Assert.equal(_name, expected_product_description, "Market should return product description");
     }
 
     function testGetProductInfoPrice() public {
         uint256 expected_product_price = 22;
-        var (_name, _description, _price) = market.getProduct(5);
+        var (_name, _description, _price) = market.getProductInfo(5);
         Assert.equal(_name, expected_product_price, "Market should return product price");
     }
 
