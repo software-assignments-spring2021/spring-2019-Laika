03a26c88a834c40f571de04e7fb3a151e483b188
owenofengland <joe215@nyu.edu>
2019-04-02 01:22:51 -0400

    Added: fixes to tests

--- test/TestMarket.sol
+++ test/TestMarket.sol
@@ -5,11 +5,18 @@ import "truffle/DeployedAddresses.sol";
 import "contracts/Market.sol";
 
 contract TestMarket {
-  Market market = Market(DeployedAddresses.Market());
-  address thisCustomer = address(this);
+  Market market;
+  address thisCustomer;
+
+  function beforeAll() public {
+    market = new Market();
+    thisCustomer = address(this);
+    market.registerProduct(5, "test product", "test description", 25);
+    market.registerCustomer(thisCustomer, "Customer", 1000);
+  }
 
   function testGetBalance() public {
-    uint256 expected = 0;
+    uint256 expected = 1000;
     Assert.equal(market.getBalance(), expected, "Market should have 0 balance");
   }
 
@@ -26,13 +33,13 @@ contract TestMarket {
   }
 
   function testGetProductInfo() public {
-    bytes32 expectedName = "Dog Food";
-    bytes32 expectedDesc = "Pupper Food";
+    bytes32 expectedName = "test product";
+    bytes32 expectedDesc = "test description";
     uint256 expectedPrice = 25;
     bytes32 returnName;
     bytes32 returnDesc;
     uint256 returnPrice;
-    (returnName, returnDesc, returnPrice) = market.getProductInfo(10);
+    (returnName, returnDesc, returnPrice) = market.getProductInfo(5);
     Assert.equal(returnName, expectedName, "Name should match");
     Assert.equal(returnDesc, expectedDesc, "Desc should match");
     Assert.equal(returnPrice, expectedPrice, "Price should match");
