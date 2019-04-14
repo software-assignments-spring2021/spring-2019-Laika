268cc74a1c21a7e25c205c6778c04889a4fcba2e
Masaki <mk5986@nyu.edu>
2019-04-14 15:39:53 -0400

    charity factory wireframe

---
+++ contracts/CharityFactory.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+
+contract CharityFactory {
+
+    address[] charities;
+
+    fuction createChildContract ()
+
+    function getShirtIntro() public view returns (string memory){
+        return (shortIntro);
+    }
+
+}
+
