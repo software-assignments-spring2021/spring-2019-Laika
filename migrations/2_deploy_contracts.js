eaea93838050d125f1368f3901e4415742facd77
Masaki <mk5986@nyu.edu>
2019-04-14 15:39:35 -0400

    change charity.sol name

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,11 @@
 var Laikoin = artifacts.require("Laikoin");
 var ExposedLaikoin = artifacts.require("ExposedLaikoin");
 var Market = artifacts.require("Market");
-var Charity = artifacts.require("Charity");
+var BrooklynAnimalAction = artifacts.require("BrooklynAnimalAction");
 
 module.exports = function(deployer) {
     deployer.deploy(Laikoin);
     deployer.deploy(ExposedLaikoin);
     deployer.deploy(Market);
-    deployer.deploy(Charity);
+    deployer.deploy(BrooklynAnimalAction);
 };
