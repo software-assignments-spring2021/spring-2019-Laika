b6af9b2c1a6c656a50ca4ef28e6b0edb83004c08
Masaki <mk5986@nyu.edu>
2019-04-15 10:52:35 -0400

    create Charity Contract function

--- contracts/CharityFactory.sol
+++ contracts/CharityFactory.sol
@@ -4,21 +4,21 @@ contract CharityFactory {
 
     address[] charities;
 
-    fuction createChildContract ()
-
-    function getShortIntro() public view returns (string memory){
-        return (shortIntro);
+    function createChildContract(string memory shortIntro, string memory about) public {
+        address newCharity = new Charity(shortIntro, about, msg.sender);
+        charities.push(newCharity);
     }
-
 }
 
 contract Charity {
     string public shortIntro;
     string public about;
+    address public owner;
 
-    function setCharity(string _shortIntro, string _about) public {
+    function setCharity(string _shortIntro, string _about, string _owner) public {
         shortIntro = _shortIntro;
         about = _about;
+        owner = _owner;
     }
 
     function getShortIntro() public view returns (string memory){
@@ -27,6 +27,10 @@ contract Charity {
 
     function getAbout() public view returns (string memory){
         return (about);
+    }
+
+    function getOwner() public view returns (string memory){
+        return (owner);
     }
 
 }
