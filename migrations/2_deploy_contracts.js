082b50431f1ee8e47d6dd23535451fc289de3466
Masaki <mk5986@nyu.edu>
2019-04-16 09:11:01 -0400

    add charity factory to deployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,10 +2,12 @@ var Laikoin = artifacts.require("Laikoin");
 var ExposedLaikoin = artifacts.require("ExposedLaikoin");
 var Market = artifacts.require("Market");
 var BrooklynAnimalAction = artifacts.require("BrooklynAnimalAction");
+var CharityFactory = artifacts.require("CharityFactory");
 
 module.exports = function(deployer) {
     deployer.deploy(Laikoin);
     deployer.deploy(ExposedLaikoin);
     deployer.deploy(Market);
     deployer.deploy(BrooklynAnimalAction);
+    deployer.deploy(CharityFactory);
 };
