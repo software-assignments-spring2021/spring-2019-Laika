e0c447d00abf58c0dafa0dfeb23844bf082f0165
Masaki <mk5986@nyu.edu>
2019-04-22 10:45:35 -0400

    set rinkeby config

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "Jolly Romeo Kneed Zeus Nevertheless Jealous Quarterbacks Overcharged Waffles Queasily"
+var mnemonic = ""
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,11 +9,13 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    ropsten: {
+    rinkeby: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/1a58d05198c348d9b29d5dd96913279d")
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/1a58d05198c348d9b29d5dd96913279d")
       },
-      network_id: 3
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
     }   
   }
 };
