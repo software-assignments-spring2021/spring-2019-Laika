fb75b599bb85c53b3f97fa8ef7cc43438ff3d52f
Rachelnarios <rnr266@Nyu.edu>
2019-04-23 12:58:04 -0400

    removed comments

--- src/js/app.js
+++ src/js/app.js
@@ -21,54 +21,7 @@ App = {
 
   return App.initWeb3();
 },
-  // init: async function() {
-  //   // Load pets.
-  //   $.getJSON('../pets.json', function(data) {
-  //     var petsRow = $('#petsRow');
-  //     var petTemplate = $('#petTemplate');
-  //
-  //     for (i = 0; i < data.length; i ++) {
-  //       petTemplate.find('.panel-title').text(data[i].name);
-  //       petTemplate.find('img').attr('src', data[i].picture);
-  //       petTemplate.find('.pet-breed').text(data[i].breed);
-  //       petTemplate.find('.pet-cost').text(data[i].cost);
-  //       petTemplate.find('.pet-location').text(data[i].location);
-  //       petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-  //       petTemplate.find('.btn-adopt').on("click",function(){
-  //         //call metamask
-  //       });
-  //
-  //       petsRow.append(petTemplate.html());
-  //     }
-  //   });
-  //
-  //   return await App.initWeb3();
-  // },
 
-  // initWeb3: async function() {
-  //     // Modern dapp browsers...
-  //     if (window.ethereum) {
-  //         App.web3Provider = window.ethereum;
-  //         try {
-  //             // Request account access
-  //             await window.ethereum.enable();
-  //         } catch (error) {
-  //             // User denied account access...
-  //             console.error("User denied account access")
-  //         }
-  //     }
-  //     // Legacy dapp browsers...
-  //     else if (window.web3) {
-  //         App.web3Provider = window.web3.currentProvider;
-  //     }
-  //     // If no injected web3 instance is detected, fall back to Ganache
-  //     else {
-  //         App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-  //     }
-  //     web3 = new Web3(App.web3Provider);
-  //
-  //     return App.initContract();
-  // },
   initWeb3: function() {
     // get web3.js - exp: Chrome w. MetaMask provides its own web3.js
     if(typeof web3 !== 'undefined'){
