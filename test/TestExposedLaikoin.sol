c3a7869328b7ec5eefff48e72ae5c1fb4ae5ccdf
Masaki <mk5986@nyu.edu>
2019-04-24 17:00:38 -0400

    test mint for exposed laikoin

--- test/TestExposedLaikoin.sol
+++ test/TestExposedLaikoin.sol
@@ -5,4 +5,10 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/ExposedLaikoin.sol";
 
 contract TestExposedLaikoin {
+    function testMint() public {
+		ExposedLaikoin user1 = new ExposedLaikoin();
+		address user1Addr = user1.getAddress();
+		user1.mint(user1Addr, 1000);
+		Assert.equal(user1.balanceOf(user1Addr), 2000, "User1 should have 2000 tokens now");
+	}
 }
