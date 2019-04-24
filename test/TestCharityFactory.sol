e902864f3bf0725dde54a714ab2a44577444e58f
Masaki <mk5986@nyu.edu>
2019-04-23 23:17:41 -0400

    test create child contract unit test

--- test/TestCharityFactory.sol
+++ test/TestCharityFactory.sol
@@ -5,5 +5,10 @@ import "truffle/DeployedAddresses.sol";
 import "contracts/CharityFactory.sol";
 
 contract TestCharityFactory {
-
+	function testCreateChildContract() public {
+        CharityFactory cf = new CharityFactory();
+        bool expected = true;
+		bool result = cf.createChildContract("some short intro about the charity", "some about page");
+		Assert.equal(expected, result, "Returned address should be equal to expected");
+	}
 }
