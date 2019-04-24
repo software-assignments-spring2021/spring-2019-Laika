bb0125bfb405e2d132995e69077578c3b48f7064
Masaki <mk5986@nyu.edu>
2019-04-23 20:02:27 -0400

    test smart contract

---
+++ test/TestCharityFactory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "contracts/CharityFactory.sol";
+
+contract TestCharityFactory {
+
+  function beforeAll() public {
+  }
+}
