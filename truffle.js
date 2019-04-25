03b7e7a39ac8399d4c539bca58e4493441e8c68d
Masaki <mk5986@nyu.edu>
2019-04-25 14:50:50 -0400

    deployed on Rinkeby

--- truffle.js
+++ truffle.js
@@ -1,17 +1,17 @@
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = ""
+var mnemonic = "ENTER YOUR MNEMONICS HERE"
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 8545,
+    //   network_id: "*" // Match any network id
+    // },
     rinkeby: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/1a58d05198c348d9b29d5dd96913279d")
+        return new HDWalletProvider(mnemonic, "ENTER YOUR INFURA ENDPOINT HERE")
       },
       network_id: 4,
       gas: 3000000,
