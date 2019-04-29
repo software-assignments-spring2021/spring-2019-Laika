263fbcdf63fa6372bcb6735a64f5a4188e9cfce5
SeanSCao <ssc543@nyu.edu>
2019-04-29 19:25:14 -0400

    Setup passport

--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@ require('./db');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const open = require('open');
 const Client = mongoose.model('Client');
 const Corp = mongoose.model('Corp');
+const passport = require('passport');
+const flash = require('connect-flash')
+const LocalStrategy = require('passport-local').Strategy;
+const bCrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -16,7 +21,36 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+const sessionOptions = {
+    secret: 'secret for signing session id',
+    saveUninitialized: false,
+    resave: false
+};
+app.use(expressSession(sessionOptions));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+passport.serializeUser(function (user, done) {
+    console.log('serializing user: ');
+    console.log(user);
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        console.log('deserializing user:', user);
+        done(err, user);
+    });
+});
+
+login(passport);
+signup(passport);
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+open('http://localhost:5000');
