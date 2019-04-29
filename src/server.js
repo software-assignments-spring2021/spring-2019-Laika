8bc463b5a5898b97f244aa434363b5d2a0412a30
SeanSCao <ssc543@nyu.edu>
2019-04-29 19:43:18 -0400

    Added baseline authentication logic for a user

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db');
 const path = require('path');
+const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const open = require('open');
@@ -15,7 +16,7 @@ const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname,'../build/contracts')));
+app.use(express.static(path.join(__dirname, '../build/contracts')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -66,6 +67,113 @@ app.get('/signout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
+
+app.get('/market', isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname + '/market.html'));
+});
+
+//Most of the passport code was from this tutorial
+//https://code.tutsplus.com/tutorials/authenticating-nodejs-applications-with-passport--cms-21619
+function login(passport) {
+
+    passport.use('login', new LocalStrategy({
+            passReqToCallback: true
+        },
+        function (req, username, password, done) {
+            // check in mongo if a user with username exists or not
+            Client.findOne({
+                    'username': username
+                },
+                function (err, user) {
+                    // In case of any error, return using the done method
+                    if (err)
+                        return done(err);
+                    // Username does not exist, log the error and redirect back
+                    if (!user) {
+                        console.log('User Not Found with username ' + username);
+                        return done(null, false, req.flash('message', 'User Not found.'));
+                    }
+                    // User exists but wrong password, log the error 
+                    if (!isValidPassword(user, password)) {
+                        console.log('Invalid Password');
+                        return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+                    }
+                    // User and password both match, return user from done method
+                    // which will be treated like success
+                    console.log("LOGGED IN");
+                    return done(null, user);
+                }
+            );
+
+        }));
+
+
+    const isValidPassword = function (user, password) {
+        return bCrypt.compareSync(password, user.password);
+    }
+
+}
+
+function signup(passport) {
+
+    passport.use('signup', new LocalStrategy({
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+        },
+        function (req, username, password, done) {
+
+            findOrCreateUser = function () {
+                // find a user in Mongo with provided username
+                Client.findOne({
+                    'username': username
+                }, function (err, user) {
+                    // In case of any error, return using the done method
+                    if (err) {
+                        console.log('Error in SignUp: ' + err);
+                        return done(err);
+                    }
+                    // already exists
+                    ifusername: String,
+                    password: String //password (user) {
+                        console.log('User already exists with username: ' + username);
+                        return done(null, false, req.flash('message', 'User Already Exists'));
+                    } else {
+                        // if there is no user with that email
+                        // create the user
+                        const newUser = new Client({
+                            username: username,
+                            password: createHash(password)
+                        });
+
+                        // save the user
+                        newUser.save(function (err) {
+                            if (err) {
+                                console.log('Error in Saving user: ' + err);
+                                throw err;
+                            }
+                            console.log('User Registration succesful');
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            };
+            // Delay the execution of findOrCreateUser and execute the method
+            // in the next tick of the event loop
+            process.nextTick(findOrCreateUser);
+        }));
+
+    // Generates hash using bCrypt
+    const createHash = function (password) {
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+    }
+
+}
+
+function isAuthenticated(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+    // if the user is not authenticated then redirect him to the home page
+    res.redirect('/');
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port);
