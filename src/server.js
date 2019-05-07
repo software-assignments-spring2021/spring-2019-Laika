40a66650454d44cfdcda30c1d52db4074f0976f8
SeanSCao <ssc543@nyu.edu>
2019-05-07 18:17:29 -0400

    test

--- src/server.js
+++ src/server.js
@@ -20,7 +20,8 @@ app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '../build/contracts')));
+app.use(express.static(path.join(__dirname, '/../pets/src')));
+app.use(express.static(path.join(__dirname, '/../build/contracts')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
