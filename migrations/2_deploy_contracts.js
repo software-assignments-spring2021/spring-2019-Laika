cb323548807abd7392546094729c0dedc856ba9b
Rachelnarios <rnr266@Nyu.edu>
2019-05-07 16:33:17 -0400

    Fix imports

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,6 +3,7 @@ var ExposedLaikoin = artifacts.require("ExposedLaikoin");
 var Market = artifacts.require("Market");
 var BrooklynAnimalAction = artifacts.require("BrooklynAnimalAction");
 var CharityFactory = artifacts.require("CharityFactory");
+var Adoption = artifacts.require("Adoption");
 
 module.exports = function(deployer) {
     deployer.deploy(Laikoin);
@@ -10,4 +11,6 @@ module.exports = function(deployer) {
     deployer.deploy(Market);
     deployer.deploy(BrooklynAnimalAction);
     deployer.deploy(CharityFactory);
+    deployer.deploy(Adoption);
+
 };
