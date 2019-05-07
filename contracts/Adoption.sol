10fdacc83c9076aa5aa7b7dc7e9dd766d18d2b1c
Rachelnarios <rnr266@Nyu.edu>
2019-05-07 16:33:44 -0400

    Fixed Adoption

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,23 +2,15 @@ pragma solidity ^0.5.0;
 
 
 contract Adoption {
-  address[16] public adopters;   // array of 16 addresses
-                                // public vars have get method - but by index!
+  address[16] public adopters;
 
-  // specify types of arg(s) AND return, and specify function access
   function adopt(uint petId) public returns (uint) {
-    require(petId >= 0 && petId <=15);   // precondition on arg
-    adopters[petId] = msg.sender;       // set to address of caller
-    return petId;                      // must return correct type
-  }
+    require(petId >= 0 && petId <=15);
+    adopters[petId] = msg.sender;
+    return petId;
+    }
 
-  // provide get for entire adopters array
-  // view is state mutability attrb - the scope of change the function is
-  // allowed to make to the Ethereum global state:
-  // 'view' functions only return values and do not modify state in any way
-  // 'pure' functions are evn more restrictive - they cannot even read the
-  // current state
   function getAdopters() public view returns (address[16] memory) {
     return adopters;
   }
-}//Adoption
+}
