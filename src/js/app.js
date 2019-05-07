8810a1f85e9a659b8477ec77d06ae9524916c1a4
Rachelnarios <rnr266@Nyu.edu>
2019-05-07 17:41:19 -0400

    Set

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,14 @@ App = {
 
   init: async function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
+    $.getJSON('../items.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
       for (i = 0; i < data.length; i ++) {
         petTemplate.find('.panel-title').text(data[i].name);
         petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
+        petTemplate.find('.pet-service').text(data[i].service);
         petTemplate.find('.pet-age').text(data[i].age);
         petTemplate.find('.pet-location').text(data[i].location);
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
